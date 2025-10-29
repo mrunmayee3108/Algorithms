6570581bf7307dbe8ecfdfffd51a6b3bd2398eb3
mrunmayee06 <msp310806@gmail.com>
2025-10-29 15:24:49 +0530

    Dijkstra's algo

--- greedy approach/dijsktra.c
+++ greedy approach/dijsktra.c
@@ -1,44 +1,45 @@
 #include<stdio.h>
-#define INF 9999
-#define MAX 100
-
+#define inf 9999
+#define MAX 50
 int main(){
-    int adjmatrix[MAX][MAX], dist[MAX], visited[MAX];
-    int n, src, i, j, count, min, u, v;
+    int n, adj[MAX][MAX], src, visited[MAX], dist[MAX], min, u, v;
     printf("Enter the number of vertices: ");
     scanf("%d", &n);
-    printf("Enter the adjacency matrix. (0 if no edge): \n");
-    for(i=0; i<n; i++){
-        for(j=0; j<n; j++){
-            scanf("%d", &adjmatrix[i][j]);
+    printf("Enter the adjacency matrix:\n");
+    for(int i=0; i<n; i++){
+        for(int j=0; j<n;j++){
+            scanf("%d", &adj[i][j]);
         }
     }
-    printf("Enter the source vertex(0 to %d): ", n-1);
+    printf("Enter the source vertex (0 to %d): ",n-1);
     scanf("%d", &src);
-    for(i=0; i<n; i++){
-        dist[i] = INF;
+    for(int i=0; i<n; i++){
         visited[i] = 0;
+        dist[i] = inf;
     }
     dist[src] = 0;
-    for(count=0; count<n-1; count++){
-        min = INF;
-        for(i=0; i<n; i++){
+    
+    // finding unvisited vertex with smallest distance
+    for(int count=0; count<n-1; count++){
+        min = inf;
+        for(int i=0; i<n; i++){
             if(!visited[i] && dist[i]<min){
                 min = dist[i];
-                u = i;                     // u stores index of the vertex with minimum distance.
+                u = i; // u is the index of the vertex with min dist.
             }
         }
         visited[u] = 1;
+        
+        // update all distances adjacent of u. 
         for(v=0; v<n; v++){
-            if(!visited[v] && adjmatrix[u][v]!=0 && dist[u]+adjmatrix[u][v]<dist[v]){
-                dist[v] = dist[u]+adjmatrix[u][v];
+            if(!visited[v] && adj[u][v]!=0 && dist[u]+adj[u][v]<dist[v]){
+                dist[v] = dist[u]+adj[u][v];
             }
         }
     }
     printf("\nVertex\tDistance from source\n");
-    for(i=0; i<n; i++){
+    for(int i=0; i<n; i++){
         printf("\n%d\t%d", i, dist[i]);
     }
     return 0;
-
 }
